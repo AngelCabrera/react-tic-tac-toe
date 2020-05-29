db2bbbb9d4b8a5bd9d85ddc0c0a371c6f9f69129
Angel Cabrera <soyangelcabrera@gmail.com>
2020-05-28 20:27:17 -0400

    Updates

---
+++ precache-manifest.5ea1d9a3e3dcdfc5a2f490ee25bb55fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2162842c789759821755aebb41fa0e54",
+    "url": "/react-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "79a85b0b5be2d2822e81",
+    "url": "/react-tic-tac-toe/static/css/main.b792a898.chunk.css"
+  },
+  {
+    "revision": "cf4df324b60ccbe3d919",
+    "url": "/react-tic-tac-toe/static/js/2.15720154.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tic-tac-toe/static/js/2.15720154.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79a85b0b5be2d2822e81",
+    "url": "/react-tic-tac-toe/static/js/main.891c56f9.chunk.js"
+  },
+  {
+    "revision": "bbf9c93bafbf1935df59",
+    "url": "/react-tic-tac-toe/static/js/runtime-main.b5a7321c.js"
+  }
+]);
